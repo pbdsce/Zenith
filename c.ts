973fbc61ba137d5fcbff198e0717bff910f75886
Nexusrex18 <lavisnj350@gmail.com>
2025-03-15 19:33:02 +0530

    build fixes

--- c.ts
+++ c.ts
@@ -1,9 +1,9 @@
-import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import cloudinary from "cloudinary";
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
   api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
 });
 
-export { cloudinary };
+export const cloudinaryV2 = cloudinary.v2;
