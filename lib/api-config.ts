e770d8c97f7cd1bdc7db60ca9d152c28123e76c1
Yash Agarwal <2004agarwalyash@gmail.com>
2026-08-08 19:24:20 +0530

    Revert "fix: update API endpoints to use /Zenith prefix"
    This reverts commit 8ca37d52f356aee623a118be4cc98c1e111277de.

--- lib/api-config.ts
+++ lib/api-config.ts
@@ -8,54 +8,54 @@ export const API_BASE_URL = '';
 
 export const API_ENDPOINTS = {
   // ===== AUTHENTICATION =====
-  register: '/zenith/api/registration',              // POST (FormData)
-  login: '/zenith/api/user/login',                   // POST (JSON)
+  register: '/api/registration',              // POST (FormData)
+  login: '/api/user/login',                   // POST (JSON)
   
   // ===== USER ENDPOINTS =====
-  userProfile: '/zenith/api/user/profile',                    // GET, PUT
-  lookingForTeam: '/zenith/api/user/looking-for-team',        // GET, PUT
-  userRsvp: '/zenith/api/user/rsvp',                          // PUT
-  userRsvpStatus: '/zenith/api/user/rsvp-status',             // GET
+  userProfile: '/api/user/profile',                    // GET, PUT
+  lookingForTeam: '/api/user/looking-for-team',        // GET, PUT
+  userRsvp: '/api/user/rsvp',                          // PUT
+  userRsvpStatus: '/api/user/rsvp-status',             // GET
   
   // ===== TEAM ENDPOINTS =====
-  createTeam: '/zenith/api/team/create',                      // POST
-  lookingForMembers: '/zenith/api/team/looking-for-members',  // GET, PUT
-  joinTeam: '/zenith/api/team/join',                          // PUT
-  joinRequest: '/zenith/api/team/join-request',               // POST, GET
-  respondToJoinRequest: (requestId: string) => `/zenith/api/team/join-request/${requestId}`,  // PUT
-  leaveTeam: '/zenith/api/team/leave',                        // PUT
-  removeMember: '/zenith/api/team/remove-member',             // PUT
-  uploadSubmission: '/zenith/api/team/upload-submission',     // POST
-  submitApplication: '/zenith/api/team/submit-application',   // POST
-  updateSubmission: '/zenith/api/team/update-submission',     // PUT
-  withdrawSubmission: '/zenith/api/team/withdraw-submission', // PUT
-  updateProblemStatement: '/zenith/api/team/update-problem-statement', // PUT
-  deleteTeam: '/zenith/api/team/delete',                      // DELETE
-  getTeam: (teamCode: string) => `/zenith/api/team/${teamCode}`,  // GET
-  getTeamMembers: (teamCode: string) => `/zenith/api/team/${teamCode}/members`,  // GET (for users looking for teams)
+  createTeam: '/api/team/create',                      // POST
+  lookingForMembers: '/api/team/looking-for-members',  // GET, PUT
+  joinTeam: '/api/team/join',                          // PUT
+  joinRequest: '/api/team/join-request',               // POST, GET
+  respondToJoinRequest: (requestId: string) => `/api/team/join-request/${requestId}`,  // PUT
+  leaveTeam: '/api/team/leave',                        // PUT
+  removeMember: '/api/team/remove-member',             // PUT
+  uploadSubmission: '/api/team/upload-submission',     // POST
+  submitApplication: '/api/team/submit-application',   // POST
+  updateSubmission: '/api/team/update-submission',     // PUT
+  withdrawSubmission: '/api/team/withdraw-submission', // PUT
+  updateProblemStatement: '/api/team/update-problem-statement', // PUT
+  deleteTeam: '/api/team/delete',                      // DELETE
+  getTeam: (teamCode: string) => `/api/team/${teamCode}`,  // GET
+  getTeamMembers: (teamCode: string) => `/api/team/${teamCode}/members`,  // GET (for users looking for teams)
   
   // ===== ADMIN ENDPOINTS =====
-  adminParticipants: '/zenith/api/admin/participants',        // GET
-  adminParticipantDetails: (id: string) => `/zenith/api/admin/participants/${id}`,  // GET
-  adminTeams: '/zenith/api/admin/teams',                      // GET
-  adminTeamDetails: (teamCode: string) => `/zenith/api/admin/teams/${teamCode}`,    // GET
-  adminUpdateTeam: (teamCode: string) => `/zenith/api/admin/teams/${teamCode}`,     // PUT
-  adminEvaluators: '/zenith/api/admin/evaluators',            // GET
-  adminAssignEvaluators: '/zenith/api/admin/evaluators/assign',  // PUT
-  adminFinalizeTeams: '/zenith/api/admin/finalize-teams',     // PUT
-  adminExport: '/zenith/api/admin/export',                    // GET
-  adminProblemStatements: '/zenith/api/admin/problem-statements',  // POST
-  adminUpdateProblemStatement: (id: string) => `/zenith/api/admin/problem-statements/${id}`,  // PUT
+  adminParticipants: '/api/admin/participants',        // GET
+  adminParticipantDetails: (id: string) => `/api/admin/participants/${id}`,  // GET
+  adminTeams: '/api/admin/teams',                      // GET
+  adminTeamDetails: (teamCode: string) => `/api/admin/teams/${teamCode}`,    // GET
+  adminUpdateTeam: (teamCode: string) => `/api/admin/teams/${teamCode}`,     // PUT
+  adminEvaluators: '/api/admin/evaluators',            // GET
+  adminAssignEvaluators: '/api/admin/evaluators/assign',  // PUT
+  adminFinalizeTeams: '/api/admin/finalize-teams',     // PUT
+  adminExport: '/api/admin/export',                    // GET
+  adminProblemStatements: '/api/admin/problem-statements',  // POST
+  adminUpdateProblemStatement: (id: string) => `/api/admin/problem-statements/${id}`,  // PUT
   
   // ===== EVALUATOR ENDPOINTS =====
-  evaluatorTeams: '/zenith/api/evaluator/teams',              // GET
-  evaluatorTeamDetails: (teamCode: string) => `/zenith/api/evaluator/teams/${teamCode}`,  // GET
-  evaluatorEvaluate: '/zenith/api/evaluator/evaluate',        // PUT
-  evaluatorUpdateEvaluation: (teamCode: string) => `/zenith/api/evaluator/evaluate/${teamCode}/update`,  // PUT
-  adminPromoteUser: '/zenith/api/admin/users/promote',        // PUT
+  evaluatorTeams: '/api/evaluator/teams',              // GET
+  evaluatorTeamDetails: (teamCode: string) => `/api/evaluator/teams/${teamCode}`,  // GET
+  evaluatorEvaluate: '/api/evaluator/evaluate',        // PUT
+  evaluatorUpdateEvaluation: (teamCode: string) => `/api/evaluator/evaluate/${teamCode}/update`,  // PUT
+  adminPromoteUser: '/api/admin/users/promote',        // PUT
   // ===== PROBLEM STATEMENT ENDPOINTS =====
-  problemStatements: '/zenith/api/problem-statements',        // GET
-  problemStatementDetails: (id: string) => `/zenith/api/problem-statements/${id}`,  // GET
+  problemStatements: '/api/problem-statements',        // GET
+  problemStatementDetails: (id: string) => `/api/problem-statements/${id}`,  // GET
 };
 
 /**
