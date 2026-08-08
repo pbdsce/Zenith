b1babec322c46f36893fea5a5af9a788b1a74d04
Yash Agarwal <2004agarwalyash@gmail.com>
2026-08-08 19:24:20 +0530

    Revert "chore: setup Next.js basePath routing for Cloudflare workers"
    This reverts commit cc8334743b91b6470245ca9c59596603e2add525.

--- next.config.js
+++ next.config.js
@@ -26,41 +26,16 @@ const nextConfig = {
       },
     },
   },
-  basePath: '/zenith',
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/zenith',
-        permanent: true,
-        basePath: false,
-      },
-    ];
-  },
   async headers() {
-    const commonHeaders = [
-      { key: 'Access-Control-Allow-Credentials', value: 'true' },
-      { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-      { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-    ];
-
     return [
       {
         source: '/api/:path*',
         headers: [
-          ...commonHeaders,
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // fallback
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
         ],
-      },
-      {
-        source: '/api/:path*',
-        has: [{ type: 'header', key: 'Origin', value: 'https://pointblank.club' }],
-        headers: [{ key: 'Access-Control-Allow-Origin', value: 'https://pointblank.club' }],
-      },
-      {
-        source: '/api/:path*',
-        has: [{ type: 'header', key: 'Origin', value: 'https://zenith.pointblank.club' }],
-        headers: [{ key: 'Access-Control-Allow-Origin', value: 'https://zenith.pointblank.club' }],
       },
     ];
   },
